5f99adea6349a796a42c2d94a93f0acdb591b783
Vander Rice <vrice@mines.edu>
2012-11-28 16:40:56 -0700

    Added main to GameGUI and made GameGUI extend JFrame.

--- src/main/GameGUI.java
+++ src/main/GameGUI.java
@@ -1,8 +1,19 @@
 package main;
 
-public class GameGUI {
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class GameGUI extends JFrame {
 	
 	private double gravity;
+	
+	public GameGUI() {
+		setLayout(new BorderLayout());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(900, 700);
+	}
 	
 	public double getGravity() {
 		return gravity;
@@ -12,5 +23,10 @@ public class GameGUI {
 		if(b.getName().equalsIgnoreCase("Earth")) {
 			gravity = 9.81;
 		}
+	}
+	
+	public static void main (String args[]) {
+		GameGUI gui = new GameGUI();
+		gui.setVisible(true);
 	}
 }
