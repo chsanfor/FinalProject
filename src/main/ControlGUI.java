650a0d6d140a779df958ca8db7603a82eca6a4d3
Chris Sanford <chsanfor@mines.edu>
2012-11-28 17:32:51 -0700

    Chris

--- src/main/ControlGUI.java
+++ src/main/ControlGUI.java
@@ -1,10 +1,30 @@
 package main;
 
-public class ControlGUI {
-	public void displayAnglePanel() {
-		
-	}
-	public void drawAngle() {
-		
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class ControlGUI extends JPanel {
+	Angle angle;
+	JSpinner angleSelect;
+//	public void displayAnglePanel() {
+//		
+//	}
+	
+//	public void drawAngle() {
+//		
+//	}
+	public ControlGUI() {
+		angle = new Angle(0);
+		setName("Missle Angle");
+		SpinnerNumberModel angleModel = new SpinnerNumberModel();
+		angleModel.setMaximum(180);
+		angleSelect = new JSpinner(angleModel);
+		angleSelect.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				angle.setAngle((Integer) angleSelect.getValue());
+			}
+		});
+		add(angleSelect);
 	}
 }
