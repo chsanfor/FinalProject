c6e0615c12053021143bc1b8695e7fad85533c38
Chris Sanford <chsanfor@mines.edu>
2012-11-30 14:49:52 -0700

    Disabled Keyboard Editing on Spinner

--- src/main/ControlGUI.java
+++ src/main/ControlGUI.java
@@ -1,13 +1,17 @@
 package main;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class ControlGUI extends JPanel {
-	Angle angle;
-	JSpinner angleSelect;
+	private Angle angle;
+	private JSpinner angleSelect;
+	private double gravity;
 //	public void displayAnglePanel() {
 //		
 //	}
@@ -20,12 +24,18 @@ public class ControlGUI extends JPanel {
 		setBorder(new TitledBorder("Missle Angle"));
 		SpinnerNumberModel angleModel = new SpinnerNumberModel();
 		angleModel.setMaximum(180);
+		angleModel.setMinimum(0);
 		angleSelect = new JSpinner(angleModel);
+//		JSpinner.DefaultEditor editor = ((JSpinner.DefaultEditor)angleSelect.getEditor());
+		JFormattedTextField tf = ((JSpinner.DefaultEditor)angleSelect.getEditor()).getTextField();
+		tf.setEditable(false);
 		angleSelect.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
 				angle.setAngle((Integer) angleSelect.getValue());
+				System.out.println(angle.getAngle());
 			}
 		});
 		add(angleSelect);
 	}
+	
 }
